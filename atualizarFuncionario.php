483d6c65a52caa7594e89efb22c7478d2b8e9228
Roddy <45442056+RoddyDev@users.noreply.github.com>
2023-02-27 13:13:15 -0300

    diagrama

--- atualizarFuncionario.php
+++ atualizarFuncionario.php
@@ -4,6 +4,8 @@
     date_default_timezone_set('America/Sao_Paulo');
 
     $st = "cm9kZHlkZXYyMDAxQA==";
+    $action = $_GET["action"];
+    $f_id = $_GET["func_id"];
     $name = $_GET["name"];
     $cpf = $_GET["cpf"];
     $proj = $_GET["id_proj"];
@@ -16,4 +18,15 @@
         echo "ERROR: Could not connect to user database.";
         exit();
     }
+
+    if ($action == "update") {
+        $query = "UPDATE `funcionarios` set `nome`='" . $name . "', `cpf`='" . $cpf . "', `id_projeto`=" . $proj . ", `id_departamento`=" . $dept . " where id = " . $f_id;
+        $handler->query($query);
+    }
+    if ($action == "remove") {
+        $query = "delete from `funcionarios` where id=" . $f_id;
+        $handler->query($query);
+    }
+
+    header("Location: https://roddydev.com/projeto");
 ?>
